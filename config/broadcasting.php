7f13884d356f49b9dbbb17f8c852089b7a910126
amr-sameh <amrsameh.develop@gmail.com>
2020-10-20 11:26:11 +0200

    configure broadcasting

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -31,16 +31,16 @@ return [
     'connections' => [
 
         'pusher' => [
-            'driver'  => 'pusher',
-            'key'     => env('PUSHER_APP_KEY'),
-            'secret'  => env('PUSHER_APP_SECRET'),
-            'app_id'  => env('PUSHER_APP_ID'),
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster'   => env('PUSHER_APP_CLUSTER'),
+                'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
-                'host'      => env('APP_HOST'),
-                'port'      => 6001,
-                'scheme'    => 'http'
+                'host' => '127.0.0.1',
+                'port' => 6001,
+                'scheme' => 'http'
             ],
         ],
 
