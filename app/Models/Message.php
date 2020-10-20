61b70d7f519c450bbf001be44b5b4d4fe6316a11
amr-sameh <amrsameh.develop@gmail.com>
2020-10-20 11:27:03 +0200

    add message model

---
+++ app/Models/Message.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "content",
+        "sender_id",
+        "receiver_id",
+    ];
+
+    protected $appends = [
+        "human_date"
+    ];
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class,'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class,'receiver_id');
+    }
+
+    public function getHumanDateAttribute()
+    {
+        return  $this->created_at->diffForHumans() ;
+    }
+}
