29f85578f0387c39ac547420869e6c0022d1f51d
amr-sameh <amrsameh.develop@gmail.com>
2020-10-20 11:26:41 +0200

    add chat routes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/chat', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/chat/general', [App\Http\Controllers\ChatController::class, 'general'])->name('chat.general');
+Route::get('/chat/private/{receiver_id}', [App\Http\Controllers\ChatController::class, 'private_chat'])->name('chat.private');
+Route::post('/chat/general/send', [App\Http\Controllers\ChatController::class, 'send_general_message'])->name('chat.general.send');
+Route::post('/chat/private/{receiver_id}/send', [App\Http\Controllers\ChatController::class, 'send_private_message'])->name('chat.private.send');
