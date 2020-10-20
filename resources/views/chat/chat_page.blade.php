ddfa04193b736c85f4394bda84c784f906e2f7f5
amr-sameh <amrsameh.develop@gmail.com>
2020-10-20 11:28:27 +0200

    send current user to chat component

--- resources/views/chat/chat_page.blade.php
+++ resources/views/chat/chat_page.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div id="app">
-        <chat-page-component></chat-page-component>
+        <chat-page-component :current-user="{{ json_encode( auth()->user() ?? null) }}"></chat-page-component>
     </div>
 
 @endsection
@@ -12,14 +12,3 @@
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 @endpush
 
-@push('js')
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="{{asset('js/jquery.nicescroll.min.js')}}" defer></script>
-
-    <script>
-        $(document).ready(function () {
-            $(".chat").niceScroll();
-
-        })
-    </script>
-@endpush
